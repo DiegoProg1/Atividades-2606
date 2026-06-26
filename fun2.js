d2d0f060b6e495b6002107f39faaa25c375c6a9d
Diego M. <2024310183@aluno.osorio.ifrs.edu.br>
2026-06-26 08:35:04 -0300

    Outra atividade aqui

---
+++ fun2.js
@@ -0,0 +1,11 @@
+
+function verificaPar(num){
+    if (num % 2 == 0) {
+        alert("O número é par");
+        return true;  
+    } else {
+        alert("O número é ímpar");
+        return false;
+    }
+}
+
